20c93d02b4199185a7b4311ab38d5c4075747120
Anas Kaarov <anasbinkurban@gmail.com>
2024-07-27 00:36:10 +0600

    feat: TechWithTim finished

--- TechWithTim/cookieclickers.py
+++ TechWithTim/cookieclickers.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-service = Service(executable_path="./chromedriver")
+service = Service(executable_path="../chromedriver")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://orteil.dashnet.org/cookieclicker/")
--- TechWithTim/main.py
+++ TechWithTim/main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-service = Service(executable_path="./chromedriver")
+service = Service(executable_path="../chromedriver")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
