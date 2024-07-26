ac6fa411972591a1403efc3ccbaca485d9f1c86a
Anas Kaarov <anasbinkurban@gmail.com>
2024-07-26 23:56:12 +0600

    feat: clicking links & navigating pages

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 service = Service(executable_path="./chromedriver")
@@ -9,8 +11,20 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
 
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
+
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.clear()
 input_element.send_keys("Tech With Tim" + Keys.ENTER)
+
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Tech With Tim"))
+)
+
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "Tech With Tim")
+link.click()
 
 time.sleep(5)
 
